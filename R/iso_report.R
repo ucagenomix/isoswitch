5b8c9fd9360d625ce4803101ea460bb5a75eaa97
ucagenomix <lebrigand@ipmc.cnrs.fr>
2022-07-19 13:55:13 +0200

    Update iso_report.R

--- R/iso_report.R
+++ R/iso_report.R
@@ -296,9 +296,9 @@ isoswitch_report_short <- function(obj, obj_assay, marker_list, gene, transcript
 #' @import ggtranscript
 #'
 #' @examples
-._isoswitch_report.junctions <- function(obj, assay, gtf_df, gene, meta){
+._isoswitch_report.junctions <- function(obj, obj_assay, gtf_df, gene, meta){
 
-  junction_counts <- obj@assays[[assay]]@counts
+  junction_counts <- obj@assays[[obj_assay]]@counts
   idx <- grep(paste("^", gene,"\\.", sep=""), rownames(junction_counts), value=TRUE)
   gene_junctions <- junction_counts[idx, ]
 
@@ -352,11 +352,11 @@ isoswitch_report_short <- function(obj, obj_assay, marker_list, gene, transcript
 #' @import glue
 #'
 #' @examples
-._isoswitch_report.umi_counts <- function(obj, gene, meta, legend=FALSE) {
+._isoswitch_report.umi_counts <- function(obj, obj_assay, gene, meta, legend=FALSE) {
 
   # get celltype_features data for each cell type
   df <- map_df(levels(obj@active.ident),
-               celltype_features, obj=obj, count_matrix=obj@assays$isoform@counts, features=meta$feature)
+               celltype_features, obj=obj, count_matrix=obj@assays[[obj_assay]]@counts, features=meta$feature)
 
   # sort cell types by expresion of major isoform - reuse sorting in meta
   celltype_order <- filter(df, transcript_id == meta$transcript_id[[1]]) %>%
