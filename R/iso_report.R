a9ec719b84473a2e4090d0481a5fa9c0b132c749
ucagenomix <lebrigand@ipmc.cnrs.fr>
2022-12-01 15:10:39 +0100

    kl

--- R/iso_report.R
+++ R/iso_report.R
@@ -175,12 +175,20 @@ isoswitch_report_short <- function(obj, obj_assay, marker_list, gene, transcript
 #'
 #' @examples
 ._isoswitch_report.dotpot2 <- function(obj, obj_assay, meta, celltype_order=NULL, switch=NULL) {
-
+ 
+  obj@active.ident <- factor(obj@active.ident, levels=celltype_order)
+  
   isofs <- meta$feature
   p2 <- DotPlot(obj, assay=obj_assay, features=isofs, scale=FALSE, cols="RdBu") + 
         scale_x_discrete(labels=meta$external_transcript_name) +
-        labs(x=NULL, y=NULL, fill="Expression", size="% cells")
-  
+        labs(x=NULL, y=NULL, fill="Expression", size="% cells") +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle=0, hjust=0.5, size=8),
+          axis.text.y = element_text(size=8),
+          legend.title = element_text(size=9),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.line = element_blank()) +
   
   
   return(p2)
